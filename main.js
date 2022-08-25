db6a6b54b9b049d71d59a7f19c50d6c55b616531
akshatranjan <73059364+akshatranjan@users.noreply.github.com>
2022-08-25 20:19:59 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -24,11 +24,26 @@ var ball = {
 function setup(){
   var canvas =  createCanvas(700,600);
   canvas.parent('canvas');
+  video = createCapture(VIDEO);
+  video.size(1200,300);
+  video.hide();
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
 }
 
+function modelLoaded()
+{
+  console.log('Model is loaded');
+}
+
+function gotPoses(results)
+{
+  console.log(results);
+}
 
 function draw(){
 
+  image(video, 0, 0, 700, 600);
  background(0); 
 
  fill("black");
